3abcbb5edbc931ee6cfbed70eb3dfff9baf0b3e3
Laiyyla <laiy4lr@gmail.com>
2026-08-16 21:56:35 -0500

    feat: Pdf generation feature added, now we can generate pdf at the moment

--- packages/database/src/test-connection.ts
+++ packages/database/src/test-connection.ts
@@ -4,16 +4,6 @@ import { branchTable } from "./schema/index.js";
 async function test() {
   const db = getDb();
   try {
-    const [newBranch] = await db
-      .insert(branchTable)
-      .values({
-        nombre: "test1",
-        serie: "0001",
-        RUC: "12345678911",
-      })
-      .returning();
-
-    console.log("sucursal creada con exito", newBranch);
     console.log("obteniendo sucursales");
     const branches = await db.select().from(branchTable);
     console.log("Sucurales obtenidas: ", branches);
