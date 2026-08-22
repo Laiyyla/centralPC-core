451e1568d4e01ac8ca030bd048960c3b701b3e71
Laiyyla <laiy4lr@gmail.com>
2026-08-22 18:31:19 -0500

    feat: First steps on web functionality

--- apps/web/src/routes/_authed/catalog/index.tsx
+++ apps/web/src/routes/_authed/catalog/index.tsx
@@ -1,9 +1,37 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { trpc } from "../../../trpc/client";
 
-export const Route = createFileRoute('/_authed/catalog/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_authed/catalog/")({
+  component: CatalogPage,
+});
 
-function RouteComponent() {
-  return <div>Hello "/_authed/catalog/"!</div>
+function CatalogPage() {
+  const [search, setSearch] = useState("");
+  const { data, isLoading, isError } = trpc.catalog.list.useQuery();
+
+  const datosFiltrados = data?.filter((item) =>
+    item.nombre.toLowerCase().includes(search.toLowerCase()),
+  );
+
+  if (isLoading) return <p>Cargando</p>;
+  if (isError) return <p>Error al obtener catalogo</p>;
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Busca por nombre"
+      />
+      <ul>
+        {datosFiltrados?.map((item) => (
+          <li key={item.id}>
+            {item.nombre} - {item.precio_ref} {item.tipo_item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
